fcf00575ad1b8bcbee2478d279c39cdd0a902178
Prateek Parmeshwar <pparmesh@andrew.cmu.edu>
2020-04-19 16:08:09 -0400

    main coded

--- Scan_Matching/scan_matching.py
+++ Scan_Matching/scan_matching.py
@@ -37,11 +37,31 @@ def scan_match(current_scan, reference_scan, init_params):
     reference_scan_xy = get_cartesian(reference_scan)
 
     # Build NDT of reference scan if not already present
-    ndt = NDT(np.asarray(reference_scan))
+    ndt = NDT(reference_scan)
     ndt.build_NDT()
     # Map the current_scan in the frame of referennce scan
     pts_dash = transform_pts(homogeneous_transformation(init_params), current_scan_xy) # (n,2)
     assert(pts_dash.shape[0] == num_curr_pts)
     # Determine the correspoding distributions these points belong in
+    score, pts_means, pts_covs = ndt.get_score_and_distributions(pts_dash)
+
+    # Optimize the score
+    old_score = -float("inf")
+    params = init_params
+    optim = NewtonOptimizer(pts_dash, np.asarray(pts_means), np.asarray(pts_covs))
+    # Iterate till convergence
+    for i in range(optim.iters):
+        optim.set_consts(params)
+        optim.set_variables(pts_dash, ndt.standard_shift(current_scan_xy), pts_means, pts_covs)
+        delta_param = optim.step()
+        params += delta_param # Update params
+        # Calculate new score
+        pts_dash = transform_pts(homogeneous_transformation(params), current_scan_xy)
+        curr_score, pts_means, pts_covs = ndt.get_score_and_distributions(pts_dash)
+        # Break early if no more changes in score 
+        # if (curr_score - old_score < 0.1):
+        #     break
+    assert(params.shape == (3,))
+    return params
 
 
